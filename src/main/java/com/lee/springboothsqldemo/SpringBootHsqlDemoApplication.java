13a2967895d2e083b80c7aebdeb68ac90d0324df
9006462 <123>
2020-01-21 13:45:25 +0800

    Initial commit

---
+++ src/main/java/com/lee/springboothsqldemo/SpringBootHsqlDemoApplication.java
@@ -0,0 +1,13 @@
+package com.lee.springboothsqldemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootHsqlDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootHsqlDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lee/springboothsqldemo/SpringBootHsqlDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lee.springboothsqldemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootHsqlDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
